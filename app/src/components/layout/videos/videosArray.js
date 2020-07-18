0a0284a8fdfca07f98c25909b1d7c29b41ad1978
ayla-marie <aylawolfgram@gmail.com>
2020-07-18 19:00:54 -0400

    resetting states of some documents

---
+++ app/src/components/layout/videos/videosArray.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const videosArray = [
+  "abc-song.mp4",
+  "art.mp4",
+  "askToPlay.mp4",
+  "numbers-1-20.mp4",
+  "pledge.mp4",
+  "science-weather.mp4"
+];
+
+export default videosArray;
