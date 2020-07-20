59b5295526d77695b45a31c06f9fababd9d07799
ayla-marie <aylawolfgram@gmail.com>
2020-07-20 19:38:00 -0400

    Videos set to display in grid. Play and Pause button working.

--- app/src/components/layout/Day.js
+++ app/src/components/layout/Day.js
@@ -1,16 +1,46 @@
 import React from "react";
 import Video from "./Video";
 import "../../App.css";
+import abcsong from "./videos/abc-song.mp4";
+import art from "./videos/art.mp4";
+import ask2play from "./videos/askToPlay.mp4";
+import nums123 from "./videos/numbers-1-20.mp4";
+import pledge from "./videos/pledge.mp4";
+import sciweather from "./videos/science-weather.mp4";
+
+const videoAtoZ = [
+  {
+    src: abcsong,
+    title: "Alphabet"
+  },
+  {
+    src: art,
+    title: "Drawing-Shapes"
+  },
+  {
+    src: ask2play,
+    title: "Friendships"
+  },
+  {
+    src: nums123,
+    title: "Counting"
+  },
+  {
+    src: pledge,
+    title: "Pledge"
+  },
+  {
+    src: sciweather,
+    title: "Weather"
+  }
+];
 
 const Day = () => {
   return (
-    <div class="day">
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
+    <div className="day">
+      {videoAtoZ.map(videoo => (
+        <Video key={videoo.title} src={videoo.src}></Video>
+      ))}
     </div>
   );
 };
--- app/src/components/layout/Video.js
+++ app/src/components/layout/Video.js
@@ -1,33 +1,64 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./progress.css";
 import "../../App.css";
-import { FaPlay, Fapause } from "react-icons/fa";
+import { FaPlay, FaPause } from "react-icons/fa";
 import { BsFullscreen } from "react-icons/bs";
 
-function Video() {
-  const fullScreen = () => {
-    return true;
-  };
+function Video(props) {
+  const { src, title } = props;
+  const [playIt, setPlayIt] = useState();
+  const [pauseIt, setPauseIt] = useState();
+  const [vidProgress, getVidProgress] = useEffect();
+  const timestampRef = React.useRef();
+  const divRef = React.useRef();
+  const progressRef = React.useRef();
+
+  //play on click
+  if (playIt) {
+    divRef.current.play();
+  }
+  //pause on click
+  if (pauseIt) {
+    divRef.current.pause();
+  }
+  //progress: not working
+  if (vidProgress) {
+    progressRef.vlue = (divRef.currentTime / divRef.duration) * 100;
+    let min = Math.floor(divRef.currentTime / 60);
+    if (min < 10) {
+      min = "0" + String(min);
+    }
+    let sec = Math.floor(divRef.currentTime % 60);
+    if (sec < 10) {
+      sec = "0" + String(sec);
+    }
+    timestampRef.innerText = `${min}: ${sec}`;
+  }
 
   return (
     <div className="masterVid">
-      <video className="vid" />
+      <label for={divRef}>{title}</label>
+      <video ref={divRef} src={src} className="vid" disablePictureInPicture />
       <div className="controls">
-        <button className="playButton">
+        <button onClick={setPlayIt} className="playButton">
           <FaPlay size={20} />
         </button>
-        <button className="fsButton" onClick={fullScreen}>
-          <BsFullscreen size={20} />
+        <button className="pauseButton" onClick={setPauseIt}>
+          <FaPause size={20} />
         </button>
         <input
           type="range"
-          class="progress"
+          className="progress"
           min="0"
           max="100"
           step="0.1"
           value="0"
+          onTimeUpdate={getVidProgress}
+          readOnly
         />
-        <span class="timestamp">00:00</span>
+        <span ref={timestampRef} className="timestamp">
+          00:00
+        </span>
       </div>
     </div>
   );
